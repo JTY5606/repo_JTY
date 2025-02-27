ffca5a1790e8e9fab17db0c5e457d884c4c1bca1
JTY5606 <sp3930@naver.com>
2025-02-27 12:09:47 +0900

    first

---
+++ JTY/src/main/java/com/JTY/JtyApplication.java
@@ -0,0 +1,13 @@
+package com.JTY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtyApplication.class, args);
+	}
+
+}
---
+++ JTY/src/test/java/com/JTY/JtyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JTY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
