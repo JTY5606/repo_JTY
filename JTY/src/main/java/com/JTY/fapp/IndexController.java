ec4d6835dc102793fff6e0603681ad494fd000cc
JTY5606 <sp3930@naver.com>
2025-02-27 14:06:07 +0900

    index

---
+++ JTY/src/main/java/com/JTY/fapp/IndexController.java
@@ -0,0 +1,23 @@
+package com.JTY.fapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = "/index")
+	public String index() {
+		return "index";
+	}
+	
+	
+	@RequestMapping(value = "/index2")
+	public String index2() {
+		return "abc/inddex";
+	}
+	@RequestMapping(value = "/aaa")
+	public String aaa() {
+		return "aaa";
+	}
+}
