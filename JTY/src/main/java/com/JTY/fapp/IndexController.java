f2f02604ef8040e260a3d45a626c653c5eb04335
JTY5606 <jty2298@gmail.com>
2025-02-27 15:03:23 +0900

    href

--- JTY/src/main/java/com/JTY/fapp/IndexController.java
+++ JTY/src/main/java/com/JTY/fapp/IndexController.java
@@ -16,8 +16,57 @@ public class IndexController {
 	public String index2() {
 		return "abc/inddex";
 	}
-	@RequestMapping(value = "/aaa")
-	public String aaa() {
-		return "aaa";
+	
+	
+	
+//	/helloWorld
+//	helloWorld
+	@RequestMapping(value = "/helloWorld")
+	public String helloWorld() {
+		return "helloWorld";
+	}
+//	/aaa/bbb
+//	bbb/aaa
+	@RequestMapping(value = "/aaa/bbb")
+	public String aaabbb() {
+		return "bbb/aaa";
+	}
+//	/xxx/yyy/zzz
+//	yyy/zzz
+	@RequestMapping(value = "/xxx/yyy/zzz")
+	public String xxxyyyzzz() {
+		return "yyy/zzz";
+	}
+//	/ttt/yyy
+//	zzz/yyy/xxx
+	@RequestMapping(value = "/ttt/yyy")
+	public String tttyyy() {
+		return "zzz/yyy/xxx";
+	}
+//	/infra/member/loginXdmForm
+//	adm/v1/tmpname/infra/member/loginForm
+	@RequestMapping(value = "/infra/member/loginXdmForm")
+	public String loginXdmForm() {
+		return "adm/v1/tmpname/infra/member/loginForm";
+	}
+//	/infra/codegroup/codeGroupXdmList
+//	adm/v1/tmpname/infra/codegroup/codeGroupList
+	@RequestMapping(value = "/infra/codegroup/codeGroupXdmList")
+	public String codeGroupXdmList() {
+		return "adm/v1/tmpname/infra/codegroup/codeGroupList";
+	}
+//	
+//	/infra/codegroup/codeGroupXdmForm
+//	adm/v1/tmpname/infra/codegroup/codeGroupForm
+
+	@RequestMapping(value = "/infra/codegroup/codeGroupXdmForm")
+	public String codeGroupXdmForm() {
+		return "adm/v1/tmpname/infra/codegroup/codeGroupForm";
+	}
+//	/infra/member/loginUsrForm
+//	usr/v1/tmpname/infra/member/loginForm
+	@RequestMapping(value = "/infra/member/loginUsrForm")
+	public String loginUsrForm() {
+		return "user/v1/tmpname/infra/member/loginForm";
 	}
 }
